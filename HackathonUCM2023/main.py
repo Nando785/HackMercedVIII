d910cd07be61c8668a234d381a38833669e974fb
Elyas Yaqubi <111923092+elyas-yaqubi@users.noreply.github.com>
2023-03-05 01:43:19 -0800

    edited main.py

--- HackathonUCM2023/main.py
+++ HackathonUCM2023/main.py
@@ -1,3 +1,22 @@
+# from flask import Flask, render_template, request, redirect, url_for
+
+# app = Flask(__name__)
+
+# compName = ""
+
+# @app.get('/')
+# def home():
+#     return render_template('index.html', html_compName = compName)
+
+# @app.route('/add', methods = ['GET', 'POST'])
+# def add():
+#     if request.method == 'POST':
+#         compName = request.form['companyName']
+#         print(compName)
+#         return redirect(url_for('home'))
+#     return render_template('add.html')
+
+
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
@@ -6,13 +25,13 @@ compName = {}
 
 @app.get('/')
 def home():
-    return render_template('index.html', html_compName = compName)
+    return render_template('index.html')
 
 @app.route('/add', methods = ['GET', 'POST'])
 def add():
     if request.method == 'POST':
         index = len(compName) + 1
-        compName[index] = request.form.get('companyName')
+        compName[index] = request.form.get("companyName")
         print(compName)
         return redirect(url_for('home'))
     return render_template('add.html')
