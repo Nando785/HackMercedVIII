db738e9a0779e314ba76a7ac75bde22525864b56
Elyas Yaqubi <111923092+elyas-yaqubi@users.noreply.github.com>
2023-03-05 23:50:15 -0800

    added if name=main in order to run the program

--- HackathonUCM2023/main.py
+++ HackathonUCM2023/main.py
@@ -16,22 +16,5 @@ def add():
         return redirect(url_for('home'))
     return render_template('add.html')
 
-
-# from flask import Flask, render_template, request, redirect, url_for
-
-# app = Flask(__name__)
-
-# compName = {}
-
-# @app.get('/')
-# def home():
-#     return render_template('index.html')
-
-# @app.route('/add', methods = ['GET', 'POST'])
-# def add():
-#     if request.method == 'POST':
-#         index = len(compName) + 1
-#         compName[index] = request.form.get("companyName")
-#         print(compName)
-#         return redirect(url_for('home'))
-#     return render_template('add.html')
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
